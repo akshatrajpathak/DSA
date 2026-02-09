e9769eeb52bba495542ac159557232d9e5627c81
akshatrajpathak <akshatrajpathak2004@gmail.com>
2026-02-09 15:10:43 +0530

    Added pattern programs

---
+++ pattern1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n=4;
+    for(int i=0;i<n;i++){
+        char a='A';
+        for(int j=0;j<n;j++){
+            
+            cout<<a;
+            a=a+1 ;
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
